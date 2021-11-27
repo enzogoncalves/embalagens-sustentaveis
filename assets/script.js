805df2e0026adb45db7d0ff2e0b940be8855ef7a
Enzo Mateus <enzomateusgonc@gmail.com>
2021-11-27 19:08:05 -0300

    começo

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
+const header = document.querySelector('#header');
+const headerHeight = header.offsetHeight;
+function headerShadow() {
+    if(window.scrollY >= headerHeight) {
+        header.classList.add('header-box-shadow');
+    } else {
+        header.classList.remove('header-box-shadow');
+    }
+}
 
+window.addEventListener('scroll', function() {
+    headerShadow();
+});
