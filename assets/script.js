e7142bbab734dad1a91ca8f6d27956146367d2c2
Enzo Mateus <enzomateusgonc@gmail.com>
2021-11-28 19:26:24 -0300

    a

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('#header');
 const headerHeight = header.offsetHeight;
 function headerShadow() {
-    if(window.scrollY >= headerHeight) {
+    if(window.scrollY >= 10) {
         header.classList.add('header-box-shadow');
     } else {
         header.classList.remove('header-box-shadow');
