3c1732737276fe96a3e171f03f036076f74bbc9f
Enzo Mateus <enzomateusgonc@gmail.com>
2021-11-29 15:02:04 -0300

    finalizado parte de conteúdo

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Adicionar sombra no header quando abaixar um pouco a página
 const header = document.querySelector('#header');
 const headerHeight = header.offsetHeight;
 function headerShadow() {
@@ -7,6 +8,13 @@ function headerShadow() {
         header.classList.remove('header-box-shadow');
     }
 }
+
+// botão de voltar para o topo da página
+const btnUpPage = document.querySelector('.toUpPage');
+
+btnUpPage.addEventListener('click', function() {
+    scrollTo(0, 0);
+});
 
 window.addEventListener('scroll', function() {
     headerShadow();
